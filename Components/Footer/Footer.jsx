1b13a757914d9b82e03468d6f2c02ccdd3b6fc58
Prithibee13 <azizprithibee1213@gmail.com>
2024-08-13 00:21:51 +0600

    small change

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
+import { faFacebook, faLinkedin, faPinterest } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const Footer = () => {
     return (
         <div className="container p-16 ml-auto mr-auto">
             <footer className="footer">
                 <nav>
                     <h1 className="font-semibold text-2xl"><span className="title">E</span>-Learning</h1>
-                   
+
                 </nav>
-               
+
                 <nav>
                     <h1 className="font-semibold text-xl">Programs</h1>
                     <a className="link link-hover">Our Programs</a>
@@ -18,7 +21,21 @@ const Footer = () => {
                     <a className="link link-hover">FAQs</a>
                     <a className="link link-hover">Contact Us</a>
                 </nav>
-                <nav></nav>
+                <nav>
+
+                    <h1 className="font-semibold text-xl">Follow Us</h1>
+                    <div className="flex  items-center">
+                        <div className="face me-2">
+                            <FontAwesomeIcon icon={faFacebook} size="2x" />
+                        </div>
+                        <div className="pin me-2">
+                            <FontAwesomeIcon icon={faPinterest} size="2x" />
+                        </div>
+                        <div className="linked me-2">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        </div>
+                    </div>
+                </nav>
             </footer>
         </div>
 
