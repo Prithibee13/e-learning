b7d846e609d22b0d436f9e33af36afdcf79233e1
Prithibee13 <azizprithibee1213@gmail.com>
2024-08-18 23:18:34 +0600

    new btn

--- Components/Reviews/Reviews.jsx
+++ Components/Reviews/Reviews.jsx
@@ -11,7 +11,7 @@ const Reviews = async() =>
                     <h1 className="text-2xl font-bold">Our Success Student Review</h1>
                 </div>
                 <div>
-                    <button>View All</button>
+                    <button className='bannerBtn'>View All</button>
                 </div>
             </article>
 
